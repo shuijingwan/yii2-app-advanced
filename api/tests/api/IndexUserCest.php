2956b249a52c2950b98e20c8041fe277f43a6541
wangqiang <wangqiang@sobey.com>
2018-04-11 11:18:41 +0800

    创建 api 测试套件。

---
+++ api/tests/api/IndexUserCest.php
@@ -0,0 +1,19 @@
+<?php
+namespace api\tests;
+use api\tests\ApiTester;
+
+class IndexUserCest
+{
+    public function _before(ApiTester $I)
+    {
+    }
+
+    public function _after(ApiTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ApiTester $I)
+    {
+    }
+}
