80b6460593b9d583a7c3bbfc8022dcd68df173f6
wangqiang <wangqiang@chinamcloud.com>
2018-07-24 13:47:05 +0800

    接口应用新增 application/json 输入格式的支持。

--- api/config/main.php
+++ api/config/main.php
@@ -15,6 +15,9 @@ return [
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-api',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'user' => [
             'identityClass' => 'api\models\User',
