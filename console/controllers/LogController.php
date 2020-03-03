513de9e1ef499b854148f33520f90c3030b68a92
wangqiang <wangqiang@chinamcloud.com>
2020-03-03 11:15:30 +0800

    日志的删除，延缓执行 10 * 60 秒(注释)的删除。

--- console/controllers/LogController.php
+++ console/controllers/LogController.php
@@ -21,40 +21,30 @@ use yii\console\ExitCode;
  */
 class LogController extends Controller
 {
-    const SLEEP_TIME = 10 * 60; // 延缓执行 10 * 60 秒
     /**
      * 日志的删除
      */
     public function actionDelete()
     {
-        /* 获取服务器当前时间，判断范围([3点，5点))，如果不在范围内，则延缓执行 10 * 60 秒，再退出 */
+        /* 获取服务器当前时间，判断范围([3点，5点))，如果不在范围内，则成功退出 */
         $time = time(); // 当前时间
         $threeTime = strtotime("today +3 hours"); // 3点
         $fiveTime = strtotime("today +5 hours"); // 5点
         if (!($time >= $threeTime && $time < $fiveTime)) {
-            // 延缓执行 10 * 60 秒
-            // sleep(static::SLEEP_TIME);
-
             return ExitCode::OK;
         }
 
-        /* 查询 1 条记录的日志时间($logTime)，基于 日志时间 升序排列，如果不存在，则延缓执行 10 * 60 秒，再退出 */
+        /* 查询 1 条记录的日志时间($logTime)，基于 日志时间 升序排列，如果不存在，则成功退出 */
         $logItem = Log::find()->select(['log_time'])->orderBy(['log_time' => SORT_ASC])->limit(1)->one();
         if (!isset($logItem)) {
-            // 延缓执行 10 * 60 秒
-            // sleep(static::SLEEP_TIME);
-
             return ExitCode::OK;
         }
         $logTime = $logItem->log_time;
 
-        /* 获取服务器当前时间的前 15 天(默认值，可自定义)的值($reservedTime：保留时间，0点)，如果 $logTime 大于等于 $reservedTime，则延缓执行 10 * 60 秒，再退出 */
+        /* 获取服务器当前时间的前 15 天(默认值，可自定义)的值($reservedTime：保留时间，0点)，如果 $logTime 大于等于 $reservedTime，则成功退出 */
         $logDeleteReservedTime = Yii::$app->params['logDeleteReservedTime'];
         $reservedTime = strtotime("today -$logDeleteReservedTime days"); // 保留时间，0点
         if ($logTime >= $reservedTime) {
-            // 延缓执行 10 * 60 秒
-            // sleep(static::SLEEP_TIME);
-
             return ExitCode::OK;
         }
 
@@ -65,10 +55,9 @@ class LogController extends Controller
             $deadlineTime = $reservedTime;
         }
 
-        /* 执行删除 SQL()，条件(日志时间 小于 $deadlineTime)，即命令行 1 次运行仅删除 1 天的日志消息，延缓执行 60 秒，再退出 */
+        /* 执行删除 SQL()，条件(日志时间 小于 $deadlineTime)，即命令行 1 次运行仅删除 1 天的日志消息，再成功退出 */
         Log::deleteAll(['<', 'log_time', $deadlineTime]);
-        // 延缓执行 60 秒
-        // sleep(60);
+
         return ExitCode::OK;
     }
 }
